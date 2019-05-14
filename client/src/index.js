d03b6d5b126fd04a0a0e30daf5f78060ee84fe41
Sam <samjwilson@gmail.com>
2019-05-14 13:03:04 -0700

    Update build command and remove service worker

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 const vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
