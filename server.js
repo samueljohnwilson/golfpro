2ec33cd6e5f9b396a62a4ae73dcba22ca03340cf
Sam <samjwilson@gmail.com>
2019-05-30 19:55:20 -0700

    Add body-parser

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const bodyParser  = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const app = express();
 
