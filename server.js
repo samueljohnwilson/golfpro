bf29e743197a5f1855cd747e7f9665e6eb882ac5
Sam <samjwilson@gmail.com>
2019-05-30 19:59:42 -0700

    Reorganize variables

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser  = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
