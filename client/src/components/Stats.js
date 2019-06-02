18944c31ab3f2b08501e13be50681c2fae3c0b08
Sam <samjwilson@gmail.com>
2019-06-02 15:57:44 -0700

    Add Card and content

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Avatar from '@material-ui/core/Avatar'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 import './Main.css';
 import './Stats.css';
@@ -7,7 +10,16 @@ class Stats extends Component {
   render() {
     return (
     <div className="container stats-container">
-      <button className="button primary-button">Add stats</button>
+      <Card className="profile-card">
+        <CardContent>
+          <Avatar 
+            alt={this.props.username} 
+            src={this.props.profilePic} 
+            className="avatar"
+          />
+          <button className="button primary-button">Add stats</button>
+        </CardContent>
+      </Card>
     </div>
     );
   }
