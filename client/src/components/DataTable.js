8a034dc38a4cc9b03e914a8fbd81f4a57e3438a9
Sam <samjwilson@gmail.com>
2019-06-02 20:22:36 -0700

    Add DataTable

---
+++ client/src/components/DataTable.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+class DataTable extends Component {
+  // constructor() {
+  //   super();
+  // }
+  render() {
+    return (
+      <div className="data-table">
+      </div>
+    );
+  }
+}
+
+export default DataTable;
