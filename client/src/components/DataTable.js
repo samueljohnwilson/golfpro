70444a95ecca92ead4d97e11fe3ac54bc092e5ef
Sam <samjwilson@gmail.com>
2019-06-05 12:44:09 -0700

    Remove unused import

--- client/src/components/DataTable.js
+++ client/src/components/DataTable.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
 class DataTable extends Component {
