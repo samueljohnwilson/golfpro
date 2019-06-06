a730e30fff8801fc55dc0dbdf06d79fcda697107
Sam <samjwilson@gmail.com>
2019-06-05 20:02:16 -0700

    Add Content component

---
+++ client/src/components/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import StatCard from './StatCard';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="content-wrapper">
+        <StatCard />
+      </div>
+    );
+  }
+}
+
+export default Content;
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 
 import Landing from './Landing';
 import Header from './Header';
-import StatCard from './StatCard';
+import Content from './Content'
 
 class Main extends Component {
   render() {
-    const isLoggedIn = false;
+    const isLoggedIn = true;
     let display;
     
     if (isLoggedIn) {
-      display = <div className="main-inner-wrapper"><Header /><StatCard /></div>
+      display = <div className="main-inner-wrapper"><Header /><Content /></div>
     } else {
       display = <div className="main-inner-wrapper"><Landing /></div>
     }
